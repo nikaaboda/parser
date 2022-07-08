e372844e53d4690072ad6412151753d8d65591b7
Nikolozi Bodaveli <nikolozibodaveli@Nikolozis-MacBook-Air.local>
2022-07-08 15:10:15 +0200

    test commit

--- devonly/reader.ts
+++ devonly/reader.ts
@@ -6,7 +6,7 @@ const reader = new Reader();
 
 console.time("parseTime");
 
-const result = reader.read("../static/StratEDI/Deasdvs/Raw/Deasdv7.txt",  "DEASDV", true)
+const result = reader.read("../static/StratEDI/Invoices/Raw/Invoice9.txt",  "INVOICE", true)
 
 // console.log(result.file)
 // console.log(JSON.stringify(result, null, 2));
